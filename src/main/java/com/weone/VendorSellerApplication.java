84d632c8080c5a58f104a0a8289cfc2d4bca23d2
ymJung <metalbird0@gmail.com>
2017-04-15 18:43:48 +0900

    initialize project

---
+++ src/main/java/com/weone/VendorSellerApplication.java
@@ -0,0 +1,12 @@
+package com.weone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorSellerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/weone/VendorSellerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.weone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VendorSellerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
